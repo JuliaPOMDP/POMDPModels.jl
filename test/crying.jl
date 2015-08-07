534717531ec47d89aef59f84e0bb87a6b6390e7a
Zachary Sunberg <sunbergzach(at)gmail.com>
2015-08-06 17:48:34 -0700

    testing rewards of running crying baby with predefined policies

---
+++ test/crying.jl
@@ -0,0 +1,39 @@
+# test simple policies on the crying baby problem
+
+using Base.Test
+
+using POMDPModels
+using POMDPToolbox
+using POMDPs
+
+problem = BabyPOMDP(-1, -10, 0.1, 0.8, 0.1, 0.9)
+
+# starve policy
+# when the baby is never fed, the reward for starting in the hungry state should be -100
+r = simulate(problem, Starve(), EmptyBelief(), eps=0.0001, initial_state=BabyState(true))
+@test_approx_eq_eps r -100.0 0.01
+
+# when the baby is never fed the average reward for starting in the full state should be -47.37
+# should take ~5 seconds
+# by the Hoeffding inequality there is less than a 1.3% chance that the average will be off by 0.5
+n = 100000
+r_sum = @parallel (+) for i in 1:n
+    simulate(problem,
+             Starve(),
+             EmptyBelief(),
+             eps=0.001,
+             rng=MersenneTwister(i),
+             initial_state=BabyState(false))
+end
+@test_approx_eq_eps r_sum/n -47.37 0.5
+
+# always feed policy
+# when the baby is always fed the reward for starting in the full state should be -10
+r = simulate(problem, AlwaysFeed(), EmptyBelief(), eps=0.0001, initial_state=BabyState(false))
+@test_approx_eq_eps r -10.0 0.01
+
+# when the baby is always fed the reward for starting in the hungry state should be -20
+r = simulate(problem, AlwaysFeed(), EmptyBelief(), eps=0.0001, initial_state=BabyState(true))
+@test_approx_eq_eps r -20.0 0.01
+
+
