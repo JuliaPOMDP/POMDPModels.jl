8b8221013406b32938f4e85d3d7da7fed779d384
Divide-By-0 <aayushgupta05@gmail.com>
2016-05-22 17:04:49 -0700

    Added dependency files

--- test/gridworld.jl
+++ test/gridworld.jl
@@ -1,3 +1,8 @@
+# Install related packages on first run
+# Pkg.clone("https://github.com/JuliaPOMDP/POMDPModels.jl")
+# Pkg.clone("https://github.com/JuliaPOMDP/POMDPToolbox.jl")
+# Pkg.clone("https://github.com/JuliaPOMDP/GenerativeModels.jl")
+
 using POMDPs
 using POMDPModels
 using POMDPToolbox
