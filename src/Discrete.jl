12b8ea29a9cf63d733cc669be60039042d22acb9
Zachary Sunberg <sunbergzach@gmail.com>
2018-08-15 13:21:28 -0700

    fixed some discrete stuff

--- src/Discrete.jl
+++ src/Discrete.jl
@@ -1,112 +1,49 @@
-using Distributions
-
-
 mutable struct DiscreteMDP <: MDP{Int64, Int64}
-    T::Array{Float64, 3} # SxAxS
+    T::Array{Float64, 3} # SPxAxS
     R::Matrix{Float64} # SxA
-    ns::Int64
-    na::Int64
     discount::Float64
-    function DiscreteMDP(T::Array{Float64, 3}, R::Matrix{Float64}, discount::Float64)
-        mdp = new()
-        # add some checks
-        mdp.ns, mdp.na = size(R, 1), size(R, 2)
-        mdp.discount = discount
-        mdp.T, mdp.R = T, R
-        return mdp
-    end
 end
 
 mutable struct DiscretePOMDP <: POMDP{Int64, Int64, Int64}
-    T::Array{Float64, 3} # SxAxS
+    T::Array{Float64, 3} # SPxAxS
     R::Matrix{Float64} # SxA
-    O::Array{Float64, 3} # OxAxS
-    ns::Int64
-    na::Int64
-    no::Int64
+    O::Array{Float64, 3} # OxAxSP
     discount::Float64
-    function DiscretePOMDP(T::Array{Float64, 3}, R::Matrix{Float64}, O::Array{Float64, 3}, discount::Float64)
-        pomdp = new()
-        # add some checks
-        pomdp.ns, pomdp.na, pomdp.no = size(R, 1), size(R, 2), size(O, 1)
-        pomdp.discount = discount
-        pomdp.T, pomdp.R, pomdp.O = T, R, O
-        return pomdp
-    end
 end
 
 const DiscreteProb = Union{DiscreteMDP, DiscretePOMDP}
 
 # Distribution Type and methods
-
-mutable struct DiscreteDistribution
-    D::Array{Float64, 3}
-    s::Int64
-    a::Int64
-    it::UnitRange{Int64}
+# XXX: this should be replaced with Categorical when https://github.com/JuliaStats/Distributions.jl/issues/743 is fixed
+struct DiscreteDistribution{P<:AbstractVector{Float64}}
+    p::P
 end
 
-iterator(d::DiscreteDistribution) = d.it
+support(d::DiscreteDistribution) = 1:length(p)
 
-pdf(d::DiscreteDistribution, sp::Int64) = d.D[sp, d.a, d.s] # T(s', a, s)
+pdf(d::DiscreteDistribution, sp::Int64) = d.p[sp] # T(s', a, s)
 
-function rand(rng::AbstractRNG, d::DiscreteDistribution)
-    cat = Weights(d.D[:,d.a,d.s])
-    return sample(rng, cat)
-end
-
-# Space  and methods
-
-mutable struct DiscreteSpace
-    it::UnitRange{Int64}
-end
-
-iterator(space::DiscreteSpace) = space.it
-
-rand(rng::AbstractRNG, space::DiscreteSpace) = rand(rng, space.it)
-
+rand(rng::AbstractRNG, d::DiscreteDistribution) = sample(rng, Weights(d.p))
 
 # MDP and POMDP common methods
 
-n_states(prob::DiscreteProb) = prob.ns
-n_actions(prob::DiscreteProb) = prob.na
+n_states(prob::DiscreteProb) = size(prob.T, 1)
+n_actions(prob::DiscreteProb) = size(prob.T, 2)
 
-state_index(::DiscreteProb, s::Int64) = s
-action_index(::DiscreteProb, a::Int64) = a
-
-states(p::DiscreteProb) = DiscreteSpace(1:p.ns)
-actions(p::DiscreteProb) = DiscreteSpace(1:p.na)
+states(p::DiscreteProb) = 1:n_states(p)
+actions(p::DiscreteProb) = 1:n_actions(p)
 
 discount(p::DiscreteProb) = p.discount
 
-function transition(prob::DiscreteProb, s::Int64, a::Int64)
-    d::DiscreteDistribution=DiscreteDistribution(prob.T,0,0,1:prob.ns)
-    d.s = s
-    d.a = a
-    return d
-end
+transition(p::DiscreteProb, s::Int64, a::Int64) = DiscreteDistribution(view(p.T, :, a, s))
 
 reward(prob::DiscreteProb, s::Int64, a::Int64) = prob.R[s, a]
-reward(prob::DiscreteProb, s::Int64, a::Int64, sp::Int64) = prob.R[s, a]
 
-mutable struct StateDist
-    cat::Vector{Float64}
-end
-initial_state_distribution(prob::DiscreteProb) = StateDist(ones(prob.ns)/prob.ns)
-rand(rng::AbstractRNG, d::StateDist) = sample(rng, Weights(d.cat))
-pdf(d::StateDist, s::Int64) = d.cat[s]
-iterator(d::StateDist) = collect(1:length(d.cat))
+initialstate_distribution(p::DiscreteProb) = DiscreteDistribution(ones(n_states(p))./n_states(p))
 
 # POMDP only methods
-observation_index(::DiscretePOMDP, o::Int64) = o
+n_observations(p::DiscreteProb) = size(p.O)
 
-n_observations(prob::DiscreteProb) = prob.no
+observations(p::DiscretePOMDP) = 1:p.n_observations(p)
 
-observations(p::DiscretePOMDP) = DiscreteSpace(1:p.no)
-
-function observation(prob::DiscretePOMDP, a::Int64, sp::Int64)
-    d=DiscreteDistribution(prob.O,0,0,1:prob.no)
-    d.s = sp
-    d.a = a
-    return d
-end
+observation(p::DiscretePOMDP, a::Int64, sp::Int64) = DiscreteDistribution(view(p.O, :, a, sp))
