894daa40fdfde338fe595c8d1cebd63d78cee9a6
Zachary Sunberg <sunbergzach@gmail.com>
2018-09-11 12:13:09 -0700

    increased to 1000 steps

--- benchmarks/lightdark.jl
+++ benchmarks/lightdark.jl
@@ -10,6 +10,13 @@ struct P <: Policy end
 POMDPs.action(::P,::Any) = rand(-1:2:1)
 POMDPs.updater(::P) = NothingUpdater()
 p = P()
-sim = RolloutSimulator(max_steps=100)
+sim = RolloutSimulator(max_steps=1000)
 
 @btime simulate($sim, $m, $p)
+
+# Output from old version with LightDark1DState
+#= 
+julia> include("benchmarks/lightdark.jl")
+  23.077 μs (1 allocation: 8 bytes)
+0.0
+=#
