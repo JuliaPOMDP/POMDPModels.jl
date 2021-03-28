3543fb8f1db1adf296b94e37d7a8a9498fbadc39
Tomas Omasta <omastto1@fel.cvut.cz>
2021-03-28 13:34:57 +0200

    Improving code according to #77

--- src/MiniHallway.jl
+++ src/MiniHallway.jl
@@ -10,79 +10,66 @@
 # The rest is available at link
 
 
-struct Observation
-    no::Int
-end
-
-struct MiniHallway <: POMDP{Int, Int, Observation}
-    T::Array{SparseCat, 2}
-    R::Array{Float64}
-    O::Array{SparseCat{Array{Observation, 1}, Array{Float64, 1}}}
-    discount::Float64
+struct MiniHallway <: POMDP{Int, Int, Int}
+    T::Array{Union{Deterministic, DiscreteUniform}, 1}
 end
 
 function MiniHallway()
-    T = Array{SparseCat, 2}(undef, 13, 3)
+    T = Array{Union{Deterministic, DiscreteUniform}, 1}(undef, 13)
 
-    T[1,1] = SparseCat((1), (1.)); T[1,2] = SparseCat((2), (1.)); T[1,3] = SparseCat((4), (1.))
-    T[2,1] = SparseCat((1), (1.)); T[2,2] = SparseCat((3), (1.)); T[2,3] = SparseCat((1), (1.))
-    T[3,1] = SparseCat((7), (1.)); T[3,2] = SparseCat((4), (1.)); T[3,3] = SparseCat((2), (1.))
-    T[4,1] = SparseCat((4), (1.)); T[4,2] = SparseCat((1), (1.)); T[4,3] = SparseCat((3), (1.))
-    T[5,1] = SparseCat((1), (1.)); T[5,2] = SparseCat((6), (1.)); T[5,3] = SparseCat((8), (1.))
-    T[6,1] = SparseCat((10), (1.)); T[6,2] = SparseCat((7), (1.)); T[6,3] = SparseCat((5), (1.))
-    T[7,1] = SparseCat((1), (1.)); T[7,2] = SparseCat((8), (1.)); T[7,3] = SparseCat((6), (1.))
-    T[8,1] = SparseCat((8), (1.)); T[8,2] = SparseCat((5), (1.)); T[8,3] = SparseCat((7), (1.))
-    T[9,1] = SparseCat((9), (1.)); T[9,2] = SparseCat((10), (1.)); T[9,3] = SparseCat((12), (1.))
-    T[10,1] = SparseCat((10), (1.)); T[10,2] = SparseCat((11), (1.)); T[10,3] = SparseCat((9), (1.))
-    T[11,1] = SparseCat((13), (1.)); T[11,2] = SparseCat((12), (1.)); T[11,3] = SparseCat((10), (1.))
-    T[12,1] = SparseCat((8), (1.)); T[12,2] = SparseCat((9), (1.)); T[12,3] = SparseCat((11), (1.))
-    T[13,1] = SparseCat((1:13), (0.083337, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.))
-    T[13,2] = SparseCat((1:13), (0.083337, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.))
-    T[13,3] = SparseCat((1:13), (0.083337, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.083333, 0.))
-    discount = 0.95
-    R = zeros(13)
-    R[13] = 1.
+    # Transitions for action 1 (and all actions in state 13) as I did not find a function for it
+    T[1] = Deterministic(1); T[2] = Deterministic(2); T[3] = Deterministic(7);
+    T[4] = Deterministic(4); T[5] = Deterministic(1); T[6] = Deterministic(10);
+    T[8] = Deterministic(8); T[7] = Deterministic(1); T[9] = Deterministic(9);
+    T[10] = Deterministic(10); T[11] = Deterministic(13); T[12] = Deterministic(8);
+    T[13] = DiscreteUniform(1, 12)
 
-    O = Array{SparseCat}(undef, 13)
-    O[1] = SparseCat([Observation(1)], [1.]); O[2] = SparseCat([Observation(2)], [1.]); O[3] = SparseCat([Observation(3)], [1.]);
-    O[4] = SparseCat([Observation(4)], [1.]); O[5] = SparseCat([Observation(5)], [1.]); O[6] = SparseCat([Observation(6)], [1.]);
-    O[7] = SparseCat([Observation(7)], [1.]); O[8] = SparseCat([Observation(8)], [1.]); O[9] = SparseCat([Observation(7)], [1.]);
-    O[10] = SparseCat([Observation(8)], [1.]); O[11] = SparseCat([Observation(5)], [1.]); O[12] = SparseCat([Observation(6)], [1.]);
-    O[13] = SparseCat([Observation(9)], [1.]);
-
-    return MiniHallway(T, R, O, discount)
+    return MiniHallway(T)
 end
 
 ##################
 # mdps interface #
 ##################
-function POMDPs.states(m::MiniHallway)::Array{Int, 1}
-    return 1:13
-end
-
-POMDPs.stateindex(m::MiniHallway, ss::Int) = ss
-POMDPs.isterminal(m::MiniHallway, ss::Int) = ss == 13
+POMDPs.states(m::MiniHallway) = 1:13
+POMDPs.stateindex(m::MiniHallway, ss::Int)::Int = ss
+POMDPs.isterminal(m::MiniHallway, ss::Int)::Bool = ss == 13
 
 function POMDPs.transition(m::MiniHallway, ss::Int, a::Int)
-    return m.T[ss, a]
+    if a == 1 || ss == 13
+        return m.T[ss]
+    else
+        if a == 2
+            return ss % 4 == 0 ? Deterministic(ss - 3) : Deterministic(ss + 1)
+        elseif a == 3
+            return (ss - 1) % 4 == 0 ? Deterministic(ss + 3) : Deterministic(ss - 1)
+        end
+    end
 end
-POMDPs.transition(m::MiniHallway, ss::Int, a::Int, sp::Int) = transition(m, ss, a)
 
-POMDPs.actions(m::MiniHallway)::Array{Int, 1} = 1:3
+POMDPs.actions(m::MiniHallway) = 1:3
 POMDPs.actionindex(m::MiniHallway, a::Int)::Int = a
 
-POMDPs.reward(m::MiniHallway, ss::Int, a::Int, sp::Int)::Float64 = m.R[sp]
-POMDPs.discount(m::MiniHallway)::Float64 = m.discount
+POMDPs.reward(m::MiniHallway, ss::Int, a::Int, sp::Int) = float(sp==13)
+POMDPs.discount(m::MiniHallway)::Float64 = 0.95
 
 ####################
 # pomdps interface #
 ####################
-POMDPs.initialstate(m::MiniHallway) = SparseCat(1:13, collect(m.T[13, 1].probs))
-POMDPs.observations(m::MiniHallway) = (Observation(i) for i in 1:9)
-POMDPs.obsindex(m::MiniHallway, o::Observation)::Int = o.no
-POMDPs.obsindex(m::MiniHallway, o::Int)::Int = o
+POMDPs.initialstate(m::MiniHallway) = m.T[13]
+POMDPs.observations(m::MiniHallway) = (i for i in 1:9)
 
-function POMDPs.observation(m::MiniHallway, a::Int, sp::Int)::SparseCat
+function POMDPs.observation(m::MiniHallway, a::Int, sp::Int)::Deterministic
+    if sp <= 8
+        return Deterministic(sp)
+    elseif sp <= 10
+        return Deterministic(sp - 2)
+    elseif sp <= 12
+        return Deterministic(sp - 6)
+    else
+        return Deterministic(9)
+    end
+
     return m.O[sp]
 end
-POMDPs.observation(m::MiniHallway, s::Int, a::Int, sp::Int)::SparseCat = observation(m, a, sp)
+POMDPs.observation(m::MiniHallway, s::Int, a::Int, sp::Int)::Deterministic = observation(m, a, sp)
+POMDPs.obsindex(m::MiniHallway, o::Int)::Int = o
--- test/minihallway.jl
+++ test/minihallway.jl
@@ -5,3 +5,5 @@ using POMDPTesting
 pomdp = MiniHallway()
 
 @test has_consistent_distributions(pomdp)
+
+@test simulate(RolloutSimulator(max_steps=100), pomdp, RandomPolicy(m)) >= 0.0
