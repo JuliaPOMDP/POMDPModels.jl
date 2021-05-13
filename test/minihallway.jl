9b0e416ed404a70e93e0eede408a2b144bd02e3c
Tomas Omasta <omastto1@fel.cvut.cz>
2021-05-13 17:48:55 +0200

    Added tests for MiniHallway

--- test/minihallway.jl
+++ test/minihallway.jl
@@ -7,3 +7,9 @@ pomdp = MiniHallway()
 @test has_consistent_distributions(pomdp)
 
 @test simulate(RolloutSimulator(max_steps=100), pomdp, RandomPolicy(pomdp)) >= 0.0
+
+@test stateindex(pomdp, 5) == 5
+@test actionindex(pomdp, 1) == 1
+@test obsindex(pomdp, 1) == 1
+
+@test reward(pomdp, 11, 1) == 1
